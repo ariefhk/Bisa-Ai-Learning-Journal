cec04c8a05756015678d9c4a3e943d9e4da4de57
ariefhk <ariefrachmanhakim1001@gmail.com>
2022-08-07 23:18:58 +0700

    add sampe set

--- Dasar -Dasar Python/Belajar/3_tipe_data.py
+++ Dasar -Dasar Python/Belajar/3_tipe_data.py
@@ -1,5 +1,4 @@
 ##### Number
-
 # 1.Integer
 number = 250
 print(number)
@@ -35,8 +34,9 @@ print(tipe_string[7:])
 print(tipe_string[:15])
 print(tipe_string[7:15])
 
-##### List
+##### List (ordered)
 var_list = [3,'test',7.5,'b',1,6,4]
+print(type(var_list))
 print(var_list[2])
 print(var_list[-2])
 print(var_list[1:4])
@@ -80,7 +80,7 @@ print(hewan)
 selected_hewan = hewan.pop(0)
 print(selected_hewan)
 
-# Operasi pada List
+## Operasi pada List
 hewan = ['kucing', 'burung', 'anjing', 'kelinci']
 angka = [4,2,8,2]
 
@@ -106,3 +106,26 @@ print(hewan+angka)
 
 # 7.Mereplikasi menggunakan operator *
 print(angka*2)
+
+##### Set (un-ordered)
+var_set = {1,8,2,'a',5,4,'b',7,4,4,4}
+print(type(var_set))
+print(var_set)
+#print(var_set[0]) #error
+
+## operasi matematika
+var_set1 = {4,1,2,3,4,4,5,6}
+var_set2 = {6,8,8,9,2,7}
+
+print(var_set1.intersection(var_set2))
+print(var_set1.union(var_set2))
+print(var_set1.difference(var_set2))
+
+# 1. Hapus elemen dengan remove
+var_set = {4,1,2,3,4,4,5,6}
+var_set.remove(6)
+print(var_set)
+
+# 2. Tambah elemen
+var_set.add(10)
+print(var_set)
