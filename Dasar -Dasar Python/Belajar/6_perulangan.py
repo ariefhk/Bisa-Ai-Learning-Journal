613fd5b018c75585750185002bca6600f507141d
ariefhk <ariefrachmanhakim1001@gmail.com>
2022-08-09 07:26:20 +0700

    add looping

---
+++ Dasar -Dasar Python/Belajar/6_perulangan.py
@@ -0,0 +1,14 @@
+# for loop
+for i in range(0, 10):
+    print('angka yang ke ', str(i))
+
+# Di List
+array = ['mobil', 'motor', 'kereta']
+for i in array:
+    print(i)
+    
+# while
+i = 1
+while i < 10:
+    print(str(i))
+    i += 1
