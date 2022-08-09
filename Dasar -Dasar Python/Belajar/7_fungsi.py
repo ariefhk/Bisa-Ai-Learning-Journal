b6730033e73d88b5ab150cc4f45847de9554638f
ariefhk <ariefrachmanhakim1001@gmail.com>
2022-08-09 10:58:03 +0700

    add function

---
+++ Dasar -Dasar Python/Belajar/7_fungsi.py
@@ -0,0 +1,17 @@
+
+
+# Void
+def calulator_tambah(angka1,angka2):
+    hasil = angka1 + angka2
+    print(hasil)
+    
+calulator_tambah(10,20)
+
+
+# Return
+def calculator_kurang(angka1, angka2):
+    hasil = angka1 -angka2
+    return hasil
+
+kurang = calculator_kurang(10,5)
+print(kurang)
